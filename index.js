216e7f514c1d72c7913e7db6bd0cbbe9118e2379
Mauricio <125153594+Mau-bar-iva@users.noreply.github.com>
2026-05-05 21:17:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const [, , method, endpoint, title, price, category] = process.argv;
+
+const partes = endpoint.split('/');
+const recurso = partes[0];
+const id = partes[1];
+
+if (method.toLowerCase() === 'get') {
+    try {
+        if (id) {
+            fetch(`${endpoint}/${id}`)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error fetching data:', error));
+        } else {
+            fetch(endpoint)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error fetching data:', error));
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+} else if (method.toLowerCase() === 'post') {
+    try {
+        if (!title || !price || !category) {
+            console.error(" Faltan datos: title, price o category");
+            process.exit(1);
+        }
+
+        const nuevoProducto = {
+            title,
+            price: Number(price),
+            category
+        };
+
+        fetch(`https://fakestoreapi.com/${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoProducto)
+        })
+            .then(response => response.json())
+            .then(data => console.log("Producto creado:", data))
+            .catch(error => console.error('Error posting data:', error));
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+} else if (method.toLowerCase() === 'delete') {
+    try {
+        if (id) {
+            fetch(`${endpoint}/${id}`)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error fetching data:', error));
+        } else {
+            fetch(endpoint)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error fetching data:', error));
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
